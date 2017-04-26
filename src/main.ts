3e1f018f595b5955cfb8ece90f115cdf4c6049cb
Jackie Zhang <viperking@163.com>
2017-04-26 23:12:55 +0800

    changed @types/jasmine version changed old angular-cli to @angular/cli

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import './polyfills.ts';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/';
 
 if (environment.production) {
