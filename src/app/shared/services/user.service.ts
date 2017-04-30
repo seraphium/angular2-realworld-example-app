1779da8716bbc8f1d5eaf8c7d78860fadceffc9e
Jackie Zhang <viperking@163.com>
2017-04-30 10:41:40 +0800

    start to try jwt authentication persistance

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -3,7 +3,9 @@
  */
 
 import {Injectable} from "@angular/core";
+import {ReplaySubject} from "rxjs/ReplaySubject";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
+
 import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import {ApiService} from "./api.service";
@@ -15,7 +17,7 @@ export class UserService{
   private currentUserSubject = new BehaviorSubject<User>(new User());
   public currentUser = this.currentUserSubject.asObservable().distinctUntilChanged();
 
-  private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+  private isAuthenticatedSubject = new ReplaySubject<boolean>(1);
   public isAuthenticated = this.isAuthenticatedSubject.asObservable();
 
   constructor(
